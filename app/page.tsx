afa9ced91956dd052745178729bb872a5de3e6c8
jose.badilla <jose.badilla@First-Factory-Inc-MacBookPro-P3997649J9.local>
2024-11-06 22:29:37 -0600

    ESlint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <section>
-      <h1 className="text-3xl text-blue-400 font-black">Hello World Arrow</h1>
+      <h1 className="text-3xl font-black text-blue-400">Hello World Arrow</h1>
     </section>
   );
 };

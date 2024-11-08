86de4b2234a177ec33936e33262ecd8c7204e352
jose.badilla <jose.badilla@First-Factory-Inc-MacBookPro-P3997649J9.local>
2024-11-08 13:31:39 -0600

    Mobile Navigation

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,21 +1,7 @@
-import { signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
-
 const Home = async () => {
   return (
     <section>
       <h1 className="h1-bold">Hello World Arrow</h1>
-
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Logout</Button>
-      </form>
     </section>
   );
 };
